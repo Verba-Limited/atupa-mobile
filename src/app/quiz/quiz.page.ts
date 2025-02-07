2bcda8734e57574ea6f2b864e76261059a1afeee
kanmitcode <oyekanmi.owolabi@mtn.com>
2025-02-07 15:01:07 +0100

    made some modification

--- src/app/quiz/quiz.page.ts
+++ src/app/quiz/quiz.page.ts
@@ -12,30 +12,6 @@ import { IonicModule } from '@ionic/angular';
 export class QuizPage {
   constructor(private router: Router) {}
 
-  categories = [
-      {
-        "id": "1",
-        "name": "Onka",
-        "description": "Numbers in Yoruba",
-        "picture": "https://example.com/images/onka.png",
-        "maxPoints": 1000
-      },
-      {
-        "id": "2",
-        "name": "Owe",
-        "description": "Yoruba Proverbs",
-        "picture": "https://example.com/images/owe.png",
-        "maxPoints": 1200
-      },
-      {
-        "id": "3",
-        "name": "Eranko",
-        "description": "Animals in Yoruba",
-        "picture": "https://example.com/images/eranko.png",
-        "maxPoints": 900
-      }
-  ]
-
   levelsPage() {
     this.router.navigate(['/levels']);
   }
