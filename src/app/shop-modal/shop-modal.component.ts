03638f09875713db0a96ff5c7e41c3255e50df14
jennifer-pecky <ugwojennifer7@gmail.com>
2025-02-13 12:22:35 +0100

    latest-change on shop modal

--- src/app/shop-modal/shop-modal.component.ts
+++ src/app/shop-modal/shop-modal.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-shop-modal',
   templateUrl: './shop-modal.component.html',
   styleUrls: ['./shop-modal.component.scss'],
+  standalone: true,
+  imports: [IonicModule],
 })
-export class ShopModalComponent  implements OnInit {
+export class ShopModalComponent {
+  constructor(private modalController: ModalController) {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  closeModal() {
+    this.modalController.dismiss();
+  }
 }
