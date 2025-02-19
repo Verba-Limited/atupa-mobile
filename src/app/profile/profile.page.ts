1cbf2680e403a8bafba66dad1c61b99f9d4be99d
jennifer-pecky <ugwojennifer7@gmail.com>
2025-02-19 08:51:05 +0100

    latest updates on profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
-interface iconTypes {
+interface MenuItem {
   icon: string;
   title: string;
-  chevron: string;
+  link?: string;
 }
 
 @Component({
@@ -12,16 +14,31 @@ interface iconTypes {
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, RouterModule, CommonModule],
 })
 export class ProfilePage {
   constructor() {}
 
-  menuItems: iconTypes[] = [
+  menuItems: MenuItem[] = [
     {
-      title: '../../assets/icon/chart-simple 1.svg',
-      chevron: '',
-      icon: '',
+      icon: '../../assets/icon/chart-simple 1.svg',
+      title: 'Leaderboard',
+      link: '/leaderboard',
+    },
+    {
+      icon: '../../assets/icon/trophy 1.svg',
+      title: 'Achievement',
+      link: '/achievement',
+    },
+    {
+      icon: '../../assets/icon/shop 1.svg',
+      title: 'Store',
+      link: '/store',
+    },
+    {
+      icon: '../../assets/icon/gear 1.svg',
+      title: 'Settings',
+      link: '/settings',
     },
   ];
 }
