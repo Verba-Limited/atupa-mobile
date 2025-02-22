4f4db2dfcb1bd7783d3474a601527df4bfdb9f4a
jennifer-pecky <ugwojennifer7@gmail.com>
2025-02-22 15:31:50 +0100

    latest update on store page

--- src/app/store/store.page.ts
+++ src/app/store/store.page.ts
@@ -4,6 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IonicModule, NavController } from '@ionic/angular';
 
+interface BoardItem {
+  amount: number;
+  buttonText: string;
+  hasVideo: boolean;
+}
+interface BundleItem {
+  title: string;
+  icon: string;
+  point: string;
+}
 @Component({
   selector: 'app-store',
   templateUrl: './store.page.html',
@@ -19,4 +29,16 @@ export class StorePage {
   navigateBack() {
     this.navCtrl.back();
   }
+  boardItems: BoardItem[] = [
+    { amount: 3, buttonText: 'Watch AD', hasVideo: true },
+    { amount: 5, buttonText: '₦5000 Buy', hasVideo: false },
+    { amount: 10, buttonText: '₦10000 Buy', hasVideo: false },
+  ];
+
+  bundlePack: BundleItem[] = [
+    { point: 'x5', title: 'Eleke ', icon: '../../assets/icon/dots.svg' },
+    { point: 'x5', title: 'Obi', icon: '../../assets/icon/almond 1.svg' },
+    { point: 'x5', title: 'Eyo Owo', icon: '../../assets/icon/coweries.svg' },
+    { point: 'x5', title: 'Ami', icon: '../../assets/icon/more 1.svg' },
+  ];
 }
