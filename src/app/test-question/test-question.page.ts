4d2481429d301fb6840715097a7ffa394ffe192f
jennifer-pecky <ugwojennifer7@gmail.com>
2025-03-06 00:42:18 +0100

    added test page

--- src/app/test-question/test-question.page.ts
+++ src/app/test-question/test-question.page.ts
@@ -12,8 +12,27 @@ import { IonicModule, NavController } from '@ionic/angular';
 })
 export class TestQuestionPage {
   selectedOption: string = '';
+  isOptionSelected: boolean = false;
   constructor(private navCtrl: NavController, private router: Router) {}
 
+  yorubaProverbs = [
+    {
+      value: 'option1',
+      label: `Kò sí ení tí ó ma gùn eṣin tí kò ní ju ìpàkó. Bí kó fẹ ju ìpàkó, eṣin tí ó ńgùn.`,
+    },
+    {
+      value: 'option2',
+      label: `Kò sí ęni tí ó ma gùn ęşin tí kò Agba ki wa loja, ki ori omo titun o wo.`,
+    },
+    {
+      value: 'option3',
+      label: `Kò sí ęni tí ó ma gùn ęşin tí kò Agba ki wa loja, ki ori omo titun o wo..`,
+    },
+    {
+      value: 'option4',
+      label: `Kò sí ęni tí ó ma gùn ęşin tí kò Agba ki wa loja, ki ori omo titun o wo..`,
+    },
+  ];
   navigateBack() {
     this.navCtrl.back();
   }
