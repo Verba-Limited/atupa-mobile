b6eda2242157791faf2e34a4581a3cff3fbe468f
jennifer-pecky <ugwojennifer7@gmail.com>
2025-03-18 00:29:08 +0100

    added setting  screen

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,16 +1,69 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 
+interface settingTypes {
+  id: number;
+  name: string;
+  arrow?: string;
+  link?: string;
+}
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
   imports: [IonicModule, RouterModule, CommonModule],
 })
-export class SettingsPage implements OnInit {
-  constructor() {}
+export class SettingsPage {
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {}
+  navigateBack() {
+    this.navCtrl.back();
+  }
+
+  subPages: settingTypes[] = [
+    {
+      id: 1,
+      name: 'Edit Profile',
+      // arrow: 'assets/icon/Rectangle223.svg',
+      link: '/edit-profile',
+    },
+    {
+      id: 2,
+      name: 'Notification',
+      // arrow: 'assets/icon/Rectangle223.svg',
+      link: '/notification',
+    },
+    {
+      id: 3,
+      name: 'Change Password',
+      // arrow: 'assets/icon/Rectangle223.svg',
+      link: '/change-password',
+    },
+    {
+      id: 4,
+      name: 'Invite a friend',
+      // arrow: 'assets/icon/Rectangle223.svg',
+      link: 'invite',
+    },
+    {
+      id: 6,
+      name: 'Help',
+      // arrow: 'assets/icon/Rectangle223.svg',
+      link: 'help',
+    },
+    {
+      id: 7,
+      name: 'Privacy Policy ',
+      // arrow: 'assets/icon/Rectangle223.svg',
+      link: 'privacy',
+    },
+    {
+      id: 8,
+      name: 'Terms and Condition',
+      // arrow: 'assets/icon/Rectangle223.svg',
+      link: 'terms',
+    },
+  ];
 }
