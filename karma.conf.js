b6ee0f597a272d49a281c978b6160a31148fa7a5
kanmitcode <oyekanmi.owolabi@mtn.com>
2025-04-13 16:30:53 +0100

    modified changes

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-process.env.CHROME_BIN = process.env.CHROME_BIN || '/usr/bin/google-chrome';
+process.env.CHROME_BIN = process.env.CHROME_BIN || '/usr/bin/chromium-browser';
 
 module.exports = function (config) {
   config.set({
